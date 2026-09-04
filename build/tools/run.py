80c76d5852aed449e07fd541445c688ec1c6edb3
LukovDev <ruslanfelitsan@gmail.com>
2026-09-04 21:17:08 +0300

    commit 04 september 2026

--- build/tools/run.py
+++ build/tools/run.py
@@ -18,6 +18,26 @@ def log(msg: str, end: str = "\n") -> None:
 
 
 # Основная функция:
+#
+# run.py - Скрипт запуска готовой программы.
+#
+
+
+# Импортируем:
+import os
+import sys
+import time
+import subprocess
+from build import Vars
+
+
+# Вывести лог отладки сборки:
+def log(msg: str, end: str = "\n") -> None:
+    if not Vars.build_lg: return
+    print(msg, end=end)
+
+
+# Основная функция:
 def main() -> None:
     config_file = f"{Vars.build_dn}/config.json"
     args, skip = [], False
@@ -42,7 +62,7 @@ def main() -> None:
     if os.path.isfile(file_path):
         log(f"\n{' '*20}{'~<[PROGRAM OUTPUT]>~':-^40}{' '*20}")
         start_time = time.time()
-        result = subprocess.run([os.path.normpath(file_path), ' '.join(args)]).returncode
+        result = subprocess.run([os.path.normpath(file_path)] + (list(args) if args else [])).returncode
         print(f"\nProcess returned: {result} <{hex(result)}>")
         print(f"Execution time: {round(time.time()-start_time, 4)}s")
     else:
